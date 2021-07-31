8e3d9871de60e6542ac3cc8fdb21d9cefeb96165
Hamza Benali <b.a.hamza1991@gmail.com>
2021-07-31 13:47:26 +0100

    add try catch blockss

--- index.js
+++ index.js
@@ -22,29 +22,56 @@ app.get("/", (req, res) => {
 
 // get all todos
 app.get("/todos", async (req, res) => {
-  const todos = await Todo.find({});
-  res.json(todos);
+  try {
+    const todos = await Todo.find({});
+    res.json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 });
 // get todo by id
 app.get("/todos/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  res.json(todo);
+  try {
+    const todo = await Todo.findById(req.params.id);
+    res.json(todo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 });
 
 // create todo
 app.post("/todos", async (req, res) => {
-  const todo = await Todo.create(req.body);
-  res.json(todo);
+  try {
+    const todo = await Todo.create(req.body);
+    res.json(todo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 });
 // update todo
 app.put("/todos/:id", async (req, res) => {
-  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new : true });
-  res.json(todo);
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(todo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 });
 // delete todo
 app.delete("/todos/:id", async (req, res) => {
-  const todo = await Todo.findByIdAndRemove(req.params.id);
-  res.json({ message: "todo has been deleted successfully" });
+  try {
+    const todo = await Todo.findByIdAndRemove(req.params.id);
+    res.json({ message: "todo has been deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
 });
 
 app.listen(port, () => {
