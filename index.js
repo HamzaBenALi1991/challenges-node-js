7e8f5a8e2c0453fd9b044d7de5b157a585f7db3b
Hamza Benali <b.a.hamza1991@gmail.com>
2021-07-31 13:57:49 +0100

    create route folder and refactor the code

--- index.js
+++ index.js
@@ -10,69 +10,16 @@ app.use(morgan("dev"));
 // body parser config
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
-// user the model
-const Todo = require("./models/todoSchema");
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my REST API!" });
 });
 
-// get all todos
-app.get("/todos", async (req, res) => {
-  try {
-    const todos = await Todo.find({});
-    res.json(todos);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error!" });
-  }
-});
-// get todo by id
-app.get("/todos/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
-    res.json(todo);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error!" });
-  }
-});
-
-// create todo
-app.post("/todos", async (req, res) => {
-  try {
-    const todo = await Todo.create(req.body);
-    res.json(todo);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error!" });
-  }
-});
-// update todo
-app.put("/todos/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.json(todo);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error!" });
-  }
-});
-// delete todo
-app.delete("/todos/:id", async (req, res) => {
-  try {
-    const todo = await Todo.findByIdAndRemove(req.params.id);
-    res.json({ message: "todo has been deleted successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error!" });
-  }
-});
+// todo api 
+const todoApi = require('./routes/todosApi');
+app.use('/api/v1', todoApi);
 
 app.listen(port, () => {
   console.log(`Application listening at http://localhost:${port}`);
